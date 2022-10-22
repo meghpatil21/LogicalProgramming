fb2642069724cf26d5fc5458f69b2f4709a48737
meghpatil21 <megh.after18@gmail.com>
2022-10-22 14:39:18 +0530

    Implemented Perfect number

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -4,20 +4,30 @@
     {
         static void Main(string[] args)
         {
-            int n1 = 0;
-            int n2 = 1;
-            int n3, i, number;
-            Console.Write("Enter the how many number of fibonacci units to print: ");
-            number = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write(n1 + " " + n2 + " ");
-            for (i = 2; i < number; ++i)
+            int sum = 0;
+            int n;
+            Console.Write("Enter the Number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            n = number;
+            for (int i = 1; i < number; i++)
             {
-                n3 = n1 + n2;
-                Console.Write(n3 + " ");
-                n1 = n2;
-                n2 = n3;
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
             }
+            if (sum == n)
+            {
+                Console.WriteLine($"\n {number} is a perfect number");
+                
+            }
+            else
+            {
+                Console.WriteLine($"\n {number} is not a perfect number");
+                
+            }
+
         }
     }
 }
