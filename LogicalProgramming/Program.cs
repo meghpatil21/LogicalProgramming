a4d53c397bad30af2015e2eeb2e61e21850e8b1c
meghpatil21 <megh.after18@gmail.com>
2022-10-22 14:32:44 +0530

    uc1_FibSeries Added

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Programs");
+            int n1 = 0;
+            int n2 = 1;
+            int n3, i, number;
+            Console.Write("Enter the how many number of fibonacci units to print: ");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < number; ++i)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
         }
     }
 }
